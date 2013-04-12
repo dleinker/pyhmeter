072a39742c9d6ac40a86ba050aeab108875daf20
Daniel E Leinker <daniel@leinker.com>
2013-04-12 14:12:28 -0400

    first step in converting function to class

--- pyhmeter.py
+++ pyhmeter.py
@@ -1,37 +1,51 @@
+from __future__ import division
 import csv
 
-def hmeter(corpus, deltah=0.0, wordcount=False):
-    """Takes a list of individual words and returns the happiness score.
-    deltah removes stop words as per Dodd paper, wordcount returns how many 
-    words matched the Dodd wordlist."""
-    # open tab-delimited word list from Dodd paper
-    dataset = csv.reader(open("DataSetS1.csv", "r"), delimiter='\t')
-    wordscores = {}
-
-    # create a dict based on the 'name' and 'happiness_average' columns
-    for row in dataset:
-        wordscores[row[0]] = row[2]
+class HMeter:
+    """HMeter implements a Hedonometer as described in the Dodd paper"""
     
-    count = 0
-    happysum = float(0)
-    deltah = float(deltah)
-    # for loops returns count of words that have a score and sum of their scores
-    for word in corpus:
-        # We're only interested if the word from our corpus is on 
-        # our list of words with scores
-        if word in wordscores:
-            # the word has a score, let's make sure the score is a float 
-            # instead of a string so we can do math with it
-            score = float(wordscores[word]) 
-            # apply methodology to remove stop words as per Dodd et al. (2011)
-            if score >= 5.0 + deltah or score <= 5.0 - deltah: 
-                happysum  += score
-                count += 1
+    # First, import the list of words from Dodd paper
+    dataset = csv.reader(open("DataSetS1.csv", "r"), delimiter='\t')
 
-    if count != 0: # divide by zero errors are sad.
-        if not wordcount:
-            return happysum / count # dividing by the count gives us the mean.
-        else:   
-            return happysum / count, count # also return count of matched words
-    else:
-        return 0.0
+    def __init__(self, wordlist):
+        self.wordlist = wordlist
+
+    def wordcount():
+        """Returns Total number of words in list. Is this needed?"""
+        pass
+
+    def wordmatches():
+        """Returns total number of words that match the Dodd paper wordlist"""
+        pass
+
+    def wordshift():
+        """Some kind of crazy function that is going to return the wordshift"""
+        pass
+
+    def hmeter(corpus, deltah=0.0):
+        """Takes a list of individual words and returns the happiness score.
+        deltah removes stop words as per Dodd paper, wordcount returns how many 
+        words matched the Dodd wordlist."""
+
+        # create a dict based on the 'name' and 'happiness_average' columns
+        wordscores = {row[0]: row[2] for row in dataset} 
+        
+        happysum = 0
+        # for loops returns count of words that have a score and sum of their scores
+        for word in corpus:
+            # We're only interested if the word from our corpus is on 
+            # our list of words with scores
+            if word in wordscores:
+                # the word has a score, let's make sure the score is a float 
+                # instead of a string so we can do math with it
+                score = float(wordscores[word]) 
+                # apply methodology to remove stop words as per Dodd et al. (2011)
+                if score >= 5.0 + deltah or score <= 5.0 - deltah: 
+                    happysum  += score
+
+        if count != 0: # divide by zero errors are sad.
+                return happysum / count # dividing by the count gives us the mean.
+        else:
+            return 0.0
+
+    def wordcount
