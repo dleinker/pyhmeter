cd005ab1d5485ae00ccd6461261248d45f3970cc
Daniel E Leinker <daniel@leinker.com>
2013-04-16 15:19:09 -0400

    removed some redundencies and scaffolding

--- pyhmeter.py
+++ pyhmeter.py
@@ -12,37 +12,28 @@ class HMeter:
         self.wordlist = wordlist
         # create a list of words that match the Dodd list of mechannical turk words
         # we only care about words that match the list, this allows us to accept filthy
-        # sources of data 
+        # sources of data without too much filtering
         self.matchlist = [word for word in self.wordlist if word in self.wordscores]
 
     def wordshift():
         """Some kind of crazy function that is going to return the wordshift"""
         pass
 
-    def testtt(self):
-        print self
-        print self.dataset
-
     def happiness_score(self, deltah=0.0):
         """Takes a list of individual words and returns the happiness score.
         deltah removes stop words as per Dodd paper, wordcount returns how many 
         words matched the Dodd wordlist."""
 
-        # create a dict based on the 'name' and 'happiness_average' columns
         count = 0
         happysum = 0
+
         # for loop returns count of words that have a score and sum of their scores
-        for word in self.wordlist:
-            # We're only interested if the word from our corpus is on 
-            # our list of words with scores
-            if word in self.wordscores:
-                # the word has a score, let's make sure the score is a float 
-                # instead of a string so we can do math with it
-                score = float(self.wordscores[word]) 
-                # apply methodology to remove stop words as per Dodd et al. (2011)
-                if score >= 5.0 + deltah or score <= 5.0 - deltah: 
-                    happysum  += score
-                    count += 1
+        for word in self.matchlist:
+            score = float(self.wordscores[word]) 
+            # apply methodology to remove stop words as per Dodd et al. (2011)
+            if score >= 5.0 + deltah or score <= 5.0 - deltah: 
+                happysum  += score
+                count += 1
         
         # TODO figure out how we should handle this better. possibly return null? raise TypeError?
         if count != 0: # divide by zero errors are sad.
